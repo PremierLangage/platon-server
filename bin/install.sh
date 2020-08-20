61ba39d2809bcd9cb2b455fe9e7361b58c3d8e80
Dominique Revuz <nimdanor@github.com>
2020-08-20 12:14:50 +0200

    added posgress initailisation

--- bin/install.sh
+++ bin/install.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+sudo -u postgres psqli <<EOF
+CREATE DATABASE django_sandbox;
+CREATE USER django WITH PASSWORD 'django_password';
+GRANT ALL PRIVILEGES ON DATABASE django_sandbox TO django;
+ALTER USER django CREATEDB;
+EOF
+
