a8e11318f13dac922063ae230793e27fd91b301b
Dominique Revuz <nimdanor@github.com>
2020-10-06 17:10:08 +0200

    Version locale

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-sudo -u postgres psqli <<EOF
-CREATE DATABASE django_sandbox;
+sudo psql -U postgres <<EOF
+CREATE DATABASE django_platon;
 CREATE USER django WITH PASSWORD 'django_password';
-GRANT ALL PRIVILEGES ON DATABASE django_sandbox TO django;
+GRANT ALL PRIVILEGES ON DATABASE django_platon TO django;
 ALTER USER django CREATEDB;
 EOF
+
+pip3 install -r requirements.txt
+python3 manage.py migrate 
 
