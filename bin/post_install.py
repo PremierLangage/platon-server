2e1649a95f2d40cc5ae1d69a8c144641acbfbf6c
Mamadou Cisse <mciissee@gmail.com>
2021-02-18 22:03:18 +0100

    chore: remove anonymous user creation from post_install script

--- bin/post_install.py
+++ bin/post_install.py
@@ -1,8 +1,7 @@
-import random
-import string
-
 import django
 from django.contrib.auth.models import User
+
+from pl_auth.enums import Role
 
 globals().update(locals())
 
@@ -12,16 +11,8 @@ try:
     user.is_staff = True
     user.is_admin = True
     user.is_superuser = True
+    user.profile.role = Role.ADMINISTRATOR
     user.save()
 except django.db.utils.IntegrityError:
     print("User 'admin' already created")
-    pass
-
-# Create anonymous
-try:
-    passwd = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-    user = User.objects.create_user(username='Anonymous', password=passwd)
-    user.save()
-except django.db.utils.IntegrityError:
-    print("User 'Anonymous' already created")
     pass
