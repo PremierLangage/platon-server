961b2b728a9a2b68b2f96a240ab0c2a02cfecf60
Mamadou Cisse <mciissee@gmail.com>
2021-04-09 11:54:15 +0200

    refactor: move pl_sandbox init logic into post_migrate signal

---
+++ apps/pl_sandbox/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'apps.pl_sandbox.apps.PlSandboxConfig'
--- apps/pl_sandbox/apps.py
+++ apps/pl_sandbox/apps.py
@@ -4,16 +4,77 @@ import sys
 
 from django.apps import AppConfig, apps
 from django.conf import settings
+from django.db.models.signals import post_migrate
 
 
 logger = logging.getLogger(__name__)
 
 
+def create_defaults(sender, *args, **kwargs):
+    # Check settings
+    if (not isinstance(settings.SANDBOX_POLL_USAGE_EVERY, int)
+            or settings.SANDBOX_POLL_USAGE_EVERY < 15):
+        raise ValueError(
+            f"Incorrect SANDBOX_POLL_USAGE_EVERY settings:{settings.SANDBOX_POLL_USAGE_EVERY}"
+        )
+    if (not isinstance(settings.SANDBOX_POLL_SPECS_EVERY, int)
+            or settings.SANDBOX_POLL_SPECS_EVERY < 300):
+        raise ValueError(
+            f"Incorrect SANDBOX_POLL_SPECS_EVERY settings:{settings.SANDBOX_POLL_SPECS_EVERY}"
+        )
+    
+    # Load needed models
+    PeriodicTask = apps.get_model(app_label='django_celery_beat', model_name='PeriodicTask')
+    IntervalSchedule = apps.get_model(
+        app_label='django_celery_beat', model_name='IntervalSchedule'
+    )
+    Sandbox = apps.get_model(app_label='pl_sandbox', model_name='Sandbox')
+    
+    # Retrieving / creating schedules needed for periodic tasks
+    usage_schedule, created = IntervalSchedule.objects.get_or_create(
+        every=settings.SANDBOX_POLL_USAGE_EVERY, period=IntervalSchedule.SECONDS
+    )
+    specs_schedule, created = IntervalSchedule.objects.get_or_create(
+        every=settings.SANDBOX_POLL_SPECS_EVERY,
+        period=IntervalSchedule.SECONDS
+    )
+    
+    # Updating every existing periodic tasks so that they correspond to
+    # SANDBOX_POLL_USAGE_EVERY & SANDBOX_POLL_SPECS_EVERY
+    PeriodicTask.objects.filter(
+        name__startswith="sandbox_poll_usage"
+    ).update(interval=usage_schedule.pk)
+    PeriodicTask.objects.filter(
+        name__startswith="sandbox_poll_specifications"
+    ).update(interval=specs_schedule.pk)
+    
+    # Checking that every sandbox has corresponding periodic tasks
+    for sandbox in Sandbox.objects.all():
+        task_name = f"sandbox_poll_usage_{sandbox.pk}"
+        try:
+            PeriodicTask.objects.get(name=task_name)
+        except PeriodicTask.DoesNotExist:
+            logger.info(f"Creating periodic task to poll usage of sandbox {sandbox}")
+            PeriodicTask.objects.create(
+                name=task_name, interval=usage_schedule, task='pl_sandbox.tasks.poll_usage',
+                args=json.dumps([sandbox.pk])
+            )
+        
+        task_name = f"sandbox_poll_specifications_{sandbox.pk}"
+        try:
+            PeriodicTask.objects.get(name=task_name)
+        except PeriodicTask.DoesNotExist:
+            logger.info(f"Creating periodic task to poll specifications of sandbox {sandbox}")
+            PeriodicTask.objects.create(
+                name=task_name, interval=specs_schedule, args=json.dumps([sandbox.pk]),
+                task='pl_sandbox.tasks.poll_specifications',
+            )
+
 
 class PlSandboxConfig(AppConfig):
     name = 'pl_sandbox'
-    
-    
+
+
     def ready(self):
         """Do some checks when django starts.
         
@@ -28,61 +89,5 @@ class PlSandboxConfig(AppConfig):
         """
         if {"makemigrations", "migrate", "reset_db"}.intersection(sys.argv):
             return
-        # Check settings
-        if (not isinstance(settings.SANDBOX_POLL_USAGE_EVERY, int)
-                or settings.SANDBOX_POLL_USAGE_EVERY < 15):
-            raise ValueError(
-                f"Incorrect SANDBOX_POLL_USAGE_EVERY settings:{settings.SANDBOX_POLL_USAGE_EVERY}"
-            )
-        if (not isinstance(settings.SANDBOX_POLL_SPECS_EVERY, int)
-                or settings.SANDBOX_POLL_SPECS_EVERY < 300):
-            raise ValueError(
-                f"Incorrect SANDBOX_POLL_SPECS_EVERY settings:{settings.SANDBOX_POLL_SPECS_EVERY}"
-            )
-        
-        # Load needed models
-        PeriodicTask = apps.get_model(app_label='django_celery_beat', model_name='PeriodicTask')
-        IntervalSchedule = apps.get_model(
-            app_label='django_celery_beat', model_name='IntervalSchedule'
-        )
-        Sandbox = apps.get_model(app_label='pl_sandbox', model_name='Sandbox')
-        
-        # Retrieving / creating schedules needed for periodic tasks
-        usage_schedule, created = IntervalSchedule.objects.get_or_create(
-            every=settings.SANDBOX_POLL_USAGE_EVERY, period=IntervalSchedule.SECONDS
-        )
-        specs_schedule, created = IntervalSchedule.objects.get_or_create(
-            every=settings.SANDBOX_POLL_SPECS_EVERY,
-            period=IntervalSchedule.SECONDS
-        )
-        
-        # Updating every existing periodic tasks so that they correspond to
-        # SANDBOX_POLL_USAGE_EVERY & SANDBOX_POLL_SPECS_EVERY
-        PeriodicTask.objects.filter(
-            name__startswith="sandbox_poll_usage"
-        ).update(interval=usage_schedule.pk)
-        PeriodicTask.objects.filter(
-            name__startswith="sandbox_poll_specifications"
-        ).update(interval=specs_schedule.pk)
-        
-        # Checking that every sandbox has corresponding periodic tasks
-        for sandbox in Sandbox.objects.all():
-            task_name = f"sandbox_poll_usage_{sandbox.pk}"
-            try:
-                PeriodicTask.objects.get(name=task_name)
-            except PeriodicTask.DoesNotExist:
-                logger.info(f"Creating periodic task to poll usage of sandbox {sandbox}")
-                PeriodicTask.objects.create(
-                    name=task_name, interval=usage_schedule, task='pl_sandbox.tasks.poll_usage',
-                    args=json.dumps([sandbox.pk])
-                )
-            
-            task_name = f"sandbox_poll_specifications_{sandbox.pk}"
-            try:
-                PeriodicTask.objects.get(name=task_name)
-            except PeriodicTask.DoesNotExist:
-                logger.info(f"Creating periodic task to poll specifications of sandbox {sandbox}")
-                PeriodicTask.objects.create(
-                    name=task_name, interval=specs_schedule, args=json.dumps([sandbox.pk]),
-                    task='pl_sandbox.tasks.poll_specifications',
-                )
+
+        post_migrate.connect(create_defaults, sender=self)
