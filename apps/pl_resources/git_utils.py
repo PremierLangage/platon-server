58432e84f25214f0db6d8ca135ebd9e1ea09267a
JUILLARD-Thomas <51150461+JUILLARD-Thomas@users.noreply.github.com>
2021-04-14 10:53:21 +0200

    docs (pl_resource) : add doc git_utils

--- apps/pl_resources/git_utils.py
+++ apps/pl_resources/git_utils.py
@@ -8,31 +8,39 @@ from django.conf import settings
 class GitUtils():
 
     @staticmethod
-    def create_repo(name):
-        """initialise new repo"""
-
+    def create_repo(repo_name):
+    """ Initialize new repo with name `repo_name` and
+        with location settings.MEDIA_ROOT. 
+        Add README file for initialize repository for gitPython
+        """
+        path_repo = os.path.join(settings.MEDIA_ROOT, repo_name)
+        
         # Create repositorie
-        repo = Repo.init(os.path.join(settings.MEDIA_ROOT, name))
+        repo = Repo.init(path_repo)
 
         # Initilize repositorie
-        filename = os.path.join(settings.MEDIA_ROOT, "README")
+        filename = os.path.join(path_repo, "README")
         open(filename, 'wb').close()
         repo.index.add(['.'])
         repo.index.commit("Initialize repo")
 
+
     @staticmethod
-    def create_branch(name, branch):
-        """create new branch"""
-        repo = Repo(os.path.join(settings.MEDIA_ROOT, name))
-        new_branch = repo.create_head(branch)
+    def create_branch(repo_name, branch_name):
+        """create new branch with name `branch_name` on repo `repo_name` 
+        with location settings.MEDIA_ROOT"""
+        repo = Repo(os.path.join(settings.MEDIA_ROOT, repo_name))
+        new_branch = repo.create_head(branch_name)
         repo.head.reference = new_branch
 
  
     @staticmethod
-    def commit(name: str, branch: str,  file_add, message_commit: str):
-        repo = Repo(os.path.join(settings.MEDIA_ROOT, name))
-        repo.index.add(['.'])
-        repo.index.commit("Initialize repo")
+    def commit(repo_name: str, branch_name: str,  files_add = ['.'], message_commit: str):
+        """add and commit file `files_add` on repo `repo_name` 
+        with location settings.MEDIA_ROOT"""
+        repo = Repo(os.path.join(settings.MEDIA_ROOT, repo_name))
+        repo.index.add(files_add)
+        repo.index.commit(message_commit)
 
     
     def tag(path_repo):
