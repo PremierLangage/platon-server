9546bed2b9ee571f0bc9b20b505d487ba4225f12
Mamadou Cisse <mciissee@gmail.com>
2021-04-26 20:38:05 +0200

    refactor: add JWT settings

--- apps/pl_auth/urls.py
+++ apps/pl_auth/urls.py
@@ -7,8 +7,8 @@ app_name = 'pl_auth'
 
 urlpatterns = [
     path('signin/', TokenObtainPairView.as_view(), name='signin'),
-    path('signin/refresh', TokenRefreshView.as_view(), name='signin-refresh'),
     path('signout/', views.SignOutView.as_view(), name='signout'),
+    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('user/', views.LoggedUserDetailView.as_view(), name='logged-user'),
     path('users/', views.UserListView.as_view(), name='user-list'),
     path('users/<str:username>', views.UserDetailView.as_view(), name='user-detail'),
--- platon/settings.py
+++ platon/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import sys
 import logging
+from datetime import timedelta
 
 import dj_database_url
 
@@ -198,6 +199,35 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
 
+# JWT
+# https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
 
 # Django Rest Framework
 # https://www.django-rest-framework.org
