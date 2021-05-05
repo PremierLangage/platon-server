0f95d284a7354604ae9c7122c1406ee2716a2695
JUILLARD-Thomas <51150461+JUILLARD-Thomas@users.noreply.github.com>
2021-05-05 16:11:10 +0200

    refactor(pl_resource): :zap: refactor endpoints

--- apps/pl_resources/urls.py
+++ apps/pl_resources/urls.py
@@ -5,44 +5,27 @@ from . import views
 app_name = 'pl_resources'
 
 urlpatterns = [
-    
-    path('', views.ResourcesList.as_view(), name='resources-list'),
-    path('<int:pk>/', views.ResourceDetail.as_view(), name='resources-detail'),
-    
-    path('<int:pk>/files/', views.FileList.as_view(), name='files-list'),
-    path('<int:pk>/files/<int:fpk>/', views.FileDetail.as_view(), name='files-detail'),
-
-    # Circle
-    path('circles/', views.CircleList.as_view(), name='circle-list'),
-    path('circles/<int:pk>/', views.CircleDetail.as_view(), name='circle-detail'),
-    path(
-        'circles/<int:pk>/tree/',
-        views.CircleResourceTree.as_view(),
-        name='circle-resources-tree'),
-
-
-    # New Endpoints
 
     # Circle's endpoint
 
     path('circles/', views.CircleList.as_view(), name='circle-list'),
     path('circles/<int:pk>/', views.CircleDetail.as_view(), name='circle-detail'),
-    path('circles/<int:pk>/register/', views.CircleDetail.as_view(), name='circle-detail'),
-    path('circles/<int:pk>/kick/', views.CircleDetail.as_view(), name='circle-detail'),
-    path('circles/<int:pk>/publish/', views.CircleDetail.as_view(), name='circle-detail'),
-    path('circles/<int:pk>/praise/', views.CircleDetail.as_view(), name='circle-detail'),
-    path('circles/<int:pk>/blame/', views.CircleDetail.as_view(), name='circle-detail'),
-    path('circles/<int:pk>/parent/', views.CircleDetail.as_view(), name='circle-detail'),
+    path('circles/<int:pk>/register/', views.CircleRegister.as_view(), name='circle-register'),
+    path('circles/<int:pk>/kick/', views.CircleKick.as_view(), name='circle-kick'),
+    path('circles/<int:pk>/publish/', views.CirclePublish.as_view(), name='circle-publish'),
+    path('circles/<int:pk>/praise/', views.CirclePraise.as_view(), name='circle-praise'),
+    path('circles/<int:pk>/blame/', views.CircleBlame.as_view(), name='circle-blame'),
+    path('circles/<int:pk>/parents/', views.CircleParents.as_view(), name='circle-parent'),
 
     # Resource's endpoints
 
     path('circles/<int:pk>/resources/', views.ResourcesList.as_view(), name='resource-list'),
-    path('circles/<int:pk>/resources/<int:pkr>/', views.ResourcesList.as_view(), name='resource-list'),
+    path('circles/<int:pk>/resources/<int:pkr>/', views.ResourceDetail.as_view(), name='resource-detail'),
   
     # Version's endpoints
 
-    path('circles/<int:pk>/resources/<int:pkr>/versions/', views.ResourcesList.as_view(), name='resource-list'),
-    path('circles/<int:pk>/resources/<int:pkr>/versions/<int:pkv>/', views.ResourcesList.as_view(), name='resource-list'),
+    path('circles/<int:pk>/resources/<int:pkr>/versions/', views.VersionList.as_view(), name='version-list'),
+    path('circles/<int:pk>/resources/<int:pkr>/versions/<int:pkv>/', views.VersionDetail.as_view(), name='version-list'),
  
 
 ]
