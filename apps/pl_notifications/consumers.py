e9488bfd4ceba96c50f2299a67299dfd842b2141
YohanRameau <yohan.rameau@gmail.com>
2021-05-27 10:30:39 +0200

    feat (pl_notification): add channel consumers methods

--- apps/pl_notifications/consumers.py
+++ apps/pl_notifications/consumers.py
@@ -1,13 +1,25 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.core.exceptions import PermissionDenied
 
 #todo
-class NotificationConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class NotificationConsumer(AsyncWebsocketConsumer):
+    """ Allow to send notification with websocket """
+    
+    notification_group_name: str
+    # sandbox_id: int
 
-    def disconnect(self, close_code):
-        pass
+    
+    async def connect(self):
+        """Connect this consumer."""
+        self.notification_group_name = self.scope["user"].username
+        await self.accept()
 
-    def receive(self, text_data):
-        pass
+
+    async def notification_message(self, event):
+        message = event['message']
+        
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
