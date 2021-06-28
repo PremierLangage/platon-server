fcbf131bf41c27c9e56a21910b98cab146ac3d40
YohanRameau <yohan.rameau@gmail.com>
2021-06-28 10:44:40 +0200

    feat(pl_notification): list all notification for an user

--- apps/pl_notifications/views.py
+++ apps/pl_notifications/views.py
@@ -7,20 +7,21 @@ from pl_core.mixins import CrudViewSet
 from .serializers import NotificationSerializer
 from .models import Notification
 
-
+User = get_user_model()
 
 class NotificationViewSet(CrudViewSet):
     
     serializer_class = NotificationSerializer
     
     def get_serializer_class(self):
-        serializer = self.get_serializer(data=self.request.data)
-        serializer.is_valid(raise_exception=True)
+        # serializer = self.get_serializer(data=self.request.data)
+        # serializer.is_valid(raise_exception=True)
         return NotificationSerializer
 
     def get_queryset(self):
-        return Notification.list_all()
-    
+        user: User = self.request.user
+        return user.notifications_users.all()
+
     @classmethod
     def as_detail(cls):
         return cls.as_view({'get': 'retrieve', 'patch': 'partial_update','delete': 'destroy'})
