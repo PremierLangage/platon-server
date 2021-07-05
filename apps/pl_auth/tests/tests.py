0b63294dc26698f855efd11990d76118395c5721
YohanRameau <yohan.rameau@gmail.com>
2021-07-06 00:12:38 +0200

    test(pl_auth): add tests for authentication

--- apps/pl_auth/tests/tests.py
+++ apps/pl_auth/tests/tests.py
@@ -1,10 +1,13 @@
+import json
+
 from django.contrib.auth import get_user_model, authenticate, login, logout
-
+from django.contrib.auth.hashers import make_password
 from django.urls import reverse
-
 from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 
 User = get_user_model()
@@ -13,25 +16,31 @@ User = get_user_model()
 class AuthTestCase(APITestCase):
 
     def setUp(self):
-        User.objects.create(username="ypicker", password="password")
-        User.objects.create(username="zpicker", password="password")
-
+        User.objects.create(username="ypicker", password=make_password("password"))
+        self.user_data = {
+            'username': "ypicker",
+            'password': "password"
+        }
+        self.sign_in_url = reverse('pl_auth:sign-in')
+        return super().setUp()
 
     def test_force_authenticate(self):
         user = User.objects.get(username='ypicker')
         client = APIClient()
         client.force_authenticate(user=user)
-        
-    def test_simple_authenticate(self):
-        user = authenticate(username='ypicker', password='password')
-        client = APIClient()
-        client.login(username='lauren', password='secret')
 
+    def user_cannot_sign_in_with_no_data(self):
+        response = self.client.post(self.sign_in_url)
+        return self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_user_can_authenticate(self):
+        response = self.client.post(self.sign_in_url, self.user_data)
+        return self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_token(self):
-        url = reverse('pl_auth:sign-in')
-        data = {'username': 'yicker', 'password': 'password'}
-        response = self.client.post(url, data, format='json')
-        print(response)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_user_cannot_authenticate_with_bad_password(self):
+        bad_data_connection = {
+            'username': "ypicker",
+            'password': "bad_password"
+        }
+        response = self.client.post(self.sign_in_url, bad_data_connection)
+        return self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
