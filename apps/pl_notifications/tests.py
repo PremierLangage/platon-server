2cc943ffcec45082c90098552ce5ee7711420c93
YohanRameau <yohan.rameau@gmail.com>
2021-07-16 12:10:39 +0200

    test(pl_notifications): cancel tests

--- apps/pl_notifications/tests.py
+++ apps/pl_notifications/tests.py
@@ -1,14 +1,37 @@
-from django.test import TestCase
-from channels.testing import WebsocketCommunicator
-from .consumers import NotificationConsumer
+# from asgiref.sync import sync_to_async
+# from django.contrib.auth import get_user_model
+# from django.contrib.auth.hashers import make_password
+# from django.urls import reverse
+
+# from channels.testing import WebsocketCommunicator
+# from rest_framework import status
+# from rest_framework.test import APITestCase
+
+# from .consumers import NotificationConsumer
 
 
-class NotificationTests(TestCase):
-    async def test_notification_consumer(self):
-        communicator = WebsocketCommunicator(NotificationConsumer.as_asgi(), "/notifications/")
-        connected, subprotocol = await communicator.connect()
-        await communicator.send_to(text_data="hello")
-        response = await communicator.receive_from()
-        assert response == "hello"
-        # Close
-        await communicator.disconnect()
+# User = get_user_model()
+
+# class NotificationTests(APITestCase):
+    
+#     def setUp(self):
+#         User.objects.create(username="ypicker", password=make_password("password"))
+#         self.user_data = {
+#             'username': "ypicker",
+#             'password': "password"
+#         }
+#         self.sign_in_url = reverse('pl_auth:sign-in')
+#         return super().setUp()
+    
+#     async def test_notification_consumer(self):
+        
+#         response = await sync_to_async(self.client.post(self.sign_in_url, self.user_data))()
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         print(response)
+        # communicator = WebsocketCommunicator(NotificationConsumer.as_asgi(), "/notifications/")
+        # connected, subprotocol = await communicator.connect()
+        # await communicator.send_to(text_data="hello")
+        # response = await communicator.receive_from()
+        # assert response == "hello"
+        # # Close
+        # await communicator.disconnect()
