88e1aaf3635c31c9dbd49f09d4a8f9fa7d15bd15
BrotherKey <uyarorhan77@gmail.com>
2022-10-04 16:53:19 +0200

    update: live url

--- apps/pl_live/views.py
+++ apps/pl_live/views.py
@@ -1,4 +1,4 @@
-import json
+import json, io, tarfile
 from asgiref.sync import async_to_sync
 
 from rest_framework.views import APIView
@@ -6,9 +6,9 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 
+from pl_loader import utils
 from pl_loader.loader import Loader
 from pl_sandbox.models import Sandbox
-
 from pl_resources.models import Resource
 
 from . import serializers
@@ -60,3 +60,43 @@ class LiveView(APIView):
             'type': resource.type,
             'content': json.loads(process_io)
         })
+    
+    def post(self, request: Request, *args, **kwargs):
+
+        resource = Resource.objects.get(pk=kwargs.get('resource_id'))
+
+        answer = json.loads(request.data.get('answer', {}))
+
+        environment = io.BytesIO()
+        with tarfile.open(fileobj=environment, mode="w:gz") as env:
+            json_file = json.dumps(answer, indent=4)
+            tar_file, tar_info = utils.string_to_tarfile('answer.json', json_file)
+            env.addfile(fileobj=tar_file, tarinfo=tar_info)
+        environment.seek(0)
+
+        sandbox = Sandbox.objects.first()
+
+        config = {
+            "commands": [
+                "ls -al > tmp.json"
+            ],
+            "environment": str(kwargs.get('env')),
+            "result_path" : "tmp.json",
+            "save": True
+        }
+
+        response = async_to_sync(sandbox.execute)(
+            request.user,
+            config,
+            environment
+        )
+
+        environment.close()
+
+        return Response({
+            'env': response.response.environment,
+            'name': resource.name,
+            'type': resource.type,
+            'answer': response.response.result
+        })
+    
